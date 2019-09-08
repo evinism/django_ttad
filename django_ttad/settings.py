1b227cbc6c6985b96b60f89f8d55e27d524f23bd
Evin Sellin <evinism@gmail.com>
2019-09-07 20:26:27 -0700

    prod w/ heroku

--- django_ttad/settings.py
+++ django_ttad/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1fxp*x9qw%imya(tomoj&$97-s-57!xf1u1n1sr3_arxpq$5=p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_ttad.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
